55e6468f061022ee9942c88df0ad8c6a9ef91fe9
Taylor Kearns <tkearns@gmail.com>
2016-04-17 21:08:43 -0400

    Deploying
    Something goes wrong when building and pushing using surge. Getting error when loading the page.
    
    ```
    syntaxError: Unexpected token <
    ```

---
+++ app/controllers/index.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  filteredList: null,
+  actions: {
+    autoComplete(param) {
+      if (param !== '') {
+        this.store.query('rental', { city: param }).then((result) => {
+          this.set('filteredList', result);
+        });
+      } else {
+        this.set('filteredList', null);
+      }
+    },
+    search(param) {
+      if (param !== '') {
+        this.store.query('rental', { city: param }).then((result) => {
+          this.set('model', result);
+        });
+      } else {
+        this.store.findAll('rental').then((result) => {
+          this.set('model', result);
+        });
+      }
+    }
+  }
+});
